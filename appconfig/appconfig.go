90d4b88327301c66def8cefeb8ba2a6f4dc3fa5d
Sascha Dibbern <sascha-code@dibbern.info>
2024-01-15 11:17:49 +0100

    Reformat

--- appconfig/appconfig.go
+++ appconfig/appconfig.go
@@ -1,23 +1,24 @@
 package appconfig
 
 import (
-    "fmt"
-    "sync"	
-    "github.com/gookit/config/v2"
-    "github.com/gookit/config/v2/yaml"
+	"fmt"
+	"sync"
+
+	"github.com/gookit/config/v2"
+	"github.com/gookit/config/v2/yaml"
 )
 
 var lock = &sync.Mutex{}
 
 type AppConfigType struct {
-	dev bool
+	dev            bool
 	backendBaseUrl string
-	hugoProject string
+	hugoProject    string
 }
 
 func assertConfigItemExistence(key string) {
 	if config.String(key) == "" {
-		panic("Configuration error: '"+key+"' is empty")
+		panic("Configuration error: '" + key + "' is empty")
 	}
 }
 
@@ -25,7 +26,7 @@ func NewAppConfig() AppConfigType {
 	config.WithOptions(config.ParseEnv)
 
 	// load flag info
-	flagkeys1 := []string{"config","dev"}
+	flagkeys1 := []string{"config", "dev"}
 	err := config.LoadFlags(flagkeys1)
 	if err != nil {
 		panic(err)
@@ -44,12 +45,12 @@ func NewAppConfig() AppConfigType {
 	assertConfigItemExistence("backendBaseUrl")
 	backendBaseUrl := config.String("backendBaseUrl")
 	fmt.Println(backendBaseUrl)
-	
+
 	assertConfigItemExistence("hugoProject")
 	hugoProject := config.String("hugoProject")
 	fmt.Println(hugoProject)
-	
-	return AppConfigType {
+
+	return AppConfigType{
 		dev,
 		backendBaseUrl,
 		hugoProject,
@@ -61,7 +62,7 @@ func (appconfig AppConfigType) BackendBaseUrl() string {
 }
 
 func (appconfig AppConfigType) HugoProject() string {
-	return appconfig.hugoProject
+	return appconfig.hugoProject + "/"
 }
 
 // AppConfig singleton
@@ -69,13 +70,13 @@ func (appconfig AppConfigType) HugoProject() string {
 var instance *AppConfigType
 
 func AppConfig() *AppConfigType {
-    if instance == nil {
-        lock.Lock()
-        defer lock.Unlock()
-        if instance == nil {
-            conf := NewAppConfig()
+	if instance == nil {
+		lock.Lock()
+		defer lock.Unlock()
+		if instance == nil {
+			conf := NewAppConfig()
 			instance = &conf
-        }
+		}
 	}
-    return instance
+	return instance
 }
