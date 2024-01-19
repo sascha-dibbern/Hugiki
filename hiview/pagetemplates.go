0d90871d2ef39e5399ed1da9f724b11bc678ef28
Sascha Dibbern <sascha-code@dibbern.info>
2024-01-19 14:45:17 +0100

    Restructure: hiview

--- hiview/pagetemplates.go
+++ hiview/pagetemplates.go
@@ -2,25 +2,18 @@ package hiview
 
 import (
 	"fmt"
-	"net/http"
 	"regexp"
 
-	"github.com/sascha-dibbern/Hugiki/hiconfig"
 	"github.com/sascha-dibbern/Hugiki/himodel"
 	"github.com/sascha-dibbern/Hugiki/hiproxy"
 )
-
-const UriPage_Edit = "/hugiki/page/edit/"
-const UriPage_EditContent = UriPage_Edit + "content/"
-const UriAction_ProxyContentPageBody = "/hugiki/action/proxy-content-page-body/"
-const UriAction_UpdateContent = "/hugiki/action/update/content/"
 
 var Filepath_From_UriPage_Edit_Regexp = regexp.MustCompile(UriPage_Edit + "(.+)/")
 var Filepath_From_UriAction_UpdateContent_Regexp = regexp.MustCompile(UriAction_UpdateContent + "(.+)/")
 var UriPage_EditContentRegexp = regexp.MustCompile(UriPage_EditContent)
 var UriAction_ProxyContentPageBodyRegexp = regexp.MustCompile(UriAction_ProxyContentPageBody)
 
-const loadhiviewHtml = `<script src="https://unpkg.com/htmx.org@1.9.10"></script>`
+const loadHtmxHtml = `<script src="https://unpkg.com/htmx.org@1.9.10"></script>`
 
 const bodyStartTagRXPS = "<body.*>\n"
 
@@ -75,7 +68,7 @@ func (generator EditContentPageGenerator) GenerateHtml(htmlInput string, context
 			%s %s
 			<!--Content polling area -->
 			<div hx-get="%s" hx-trigger="every 1s">
-		`, matchedbodystart, loadhiviewHtml, pollingUrl),
+		`, matchedbodystart, loadHtmxHtml, pollingUrl),
 	)
 
 	modifiedcontrolareastring := bodyEndTagRegexp.ReplaceAllString(
@@ -97,44 +90,4 @@ func (generator EditContentPageGenerator) GenerateHtml(htmlInput string, context
 	`, update_uri, Render_EditContentText(markdown_content, filepath))
 
 	return result + "</div>" + bodyEndTagRXPS
-}
-
-/*
- * <body>...</body> -> <body><hugiki>...</hugiki><body>
- *  <div> <!-- Content polling -->
- *   ...
- *  </div>
- *  <div> <!-- Hugiki control -->
- *   ...
- *  </div>
- * </body>
- */
-
-func MakeHugikiWorkpageHtml(htmlInput string, request *http.Request) string {
-	backendBaseUrl := hiconfig.AppConfig().BackendBaseUrl()
-	pollingUrl := backendBaseUrl + request.URL.RequestURI()
-
-	bodyStartTagRXP := "<body.*>\n"
-	bodyEndTag := "</body>"
-
-	rexp1 := regexp.MustCompile(bodyStartTagRXP)
-	matched := rexp1.FindString(htmlInput)
-	result1 := rexp1.ReplaceAllString(htmlInput, matched+loadhiviewHtml+"<!--Content polling area --><div hx-get=\""+pollingUrl+"\" hx-trigger=\"every 1s\">")
-
-	rexp2 := regexp.MustCompile(bodyEndTag)
-	result2 := rexp2.ReplaceAllString(result1, "</div><!-- Hugiki control area --><div>")
-
-	result3 := insertWorkareaHtml(result2, request)
-
-	return result3 + "</div>" + bodyEndTag
-}
-
-func insertWorkareaHtml(htmlInput string, frontendReq *http.Request) string {
-	workarea := `
-	<h3>edit<h3></br>	
-	<h3>create sibling page<h3></br>
-	<h3>create child page<h3>
-	`
-
-	return htmlInput + workarea
 }
---
+++ hiview/uridefinitions.go
@@ -0,0 +1,6 @@
+package hiview
+
+const UriPage_Edit = "/hugiki/page/edit/"
+const UriPage_EditContent = UriPage_Edit + "content/"
+const UriAction_ProxyContentPageBody = "/hugiki/action/proxy-content-page-body/"
+const UriAction_UpdateContent = "/hugiki/action/update/content/"
