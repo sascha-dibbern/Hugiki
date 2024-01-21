14c26c1c0c0d133d4b9a4c48cd199d4f570baed6
Sascha Dibbern <sascha-code@dibbern.info>
2024-01-22 00:14:21 +0100

    Added mode uris

--- hiuri/uridefinitions.go
+++ hiuri/uridefinitions.go
@@ -4,5 +4,13 @@ const UriPage_Root = "/"
 const UriPage_Edit = "/hugiki/page/edit/"
 const UriPage_EditContent = UriPage_Edit + "content/"
 const UriPage_Config = "/hugiki/page/config/"
+
+// Edit content actions
 const UriAction_ProxyContentPageBody = "/hugiki/action/proxy-content-page-body/"
 const UriAction_UpdateContent = "/hugiki/action/update/content/"
+
+// Main page actions
+const UriAction_ConfigMode = "/hugiki/action/mode/config/"
+const UriAction_NavigationMode = "/hugiki/action/mode/navigation/"
+const UriAction_GitMode = "/hugiki/action/mode/git/"
+const UriAction_HelpMode = "/hugiki/action/mode/help/"
