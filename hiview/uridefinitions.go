1d3fdb32eba902e51042d6999f90317541f1d69a
Sascha Dibbern <sascha-code@dibbern.info>
2024-01-21 21:05:44 +0100

    Refector. central URI definitions

--- hiview/uridefinitions.go
+++
@@ -1,6 +0,0 @@
-package hiview
-
-const UriPage_Edit = "/hugiki/page/edit/"
-const UriPage_EditContent = UriPage_Edit + "content/"
-const UriAction_ProxyContentPageBody = "/hugiki/action/proxy-content-page-body/"
-const UriAction_UpdateContent = "/hugiki/action/update/content/"
