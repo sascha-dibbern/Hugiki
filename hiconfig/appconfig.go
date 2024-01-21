385dd9ea82477ab2ff83efa2e8047835dadc962d
Sascha Dibbern <sascha-code@dibbern.info>
2024-01-22 00:13:44 +0100

    Refactor: appconfig

--- hiconfig/appconfig.go
+++ hiconfig/appconfig.go
@@ -11,10 +11,8 @@ import (
 var lock = &sync.Mutex{}
 
 type AppConfigType struct {
-	dev            bool
-	backendBaseUrl string
-	hugoProject    string
-	gitCommand     string
+	boolconfigs   map[string]bool
+	stringconfigs map[string]string
 }
 
 func assertConfigItemExistence(key string) {
@@ -25,6 +23,9 @@ func assertConfigItemExistence(key string) {
 
 func NewAppConfig() AppConfigType {
 	config.WithOptions(config.ParseEnv)
+
+	stringmap := make(map[string]string)
+	boolmap := make(map[string]bool)
 
 	// load flag info
 	flagkeys1 := []string{"config", "dev"}
@@ -41,22 +42,42 @@ func NewAppConfig() AppConfigType {
 		panic(err)
 	}
 
-	dev := config.Bool("dev")
-
-	assertConfigItemExistence("backendBaseUrl")
-	backendBaseUrl := config.String("backendBaseUrl")
-	fmt.Println(backendBaseUrl)
-
 	assertConfigItemExistence("hugoProject")
-	hugoProject := config.String("hugoProject")
-	fmt.Println(hugoProject)
 
-	return AppConfigType{
-		dev,
-		backendBaseUrl,
-		hugoProject,
-		"git",
+	appconfig := AppConfigType{boolmap, stringmap}
+	appconfig.setBool("dev", config.Bool("dev"))
+	appconfig.setString("hugoproject", config.String("hugoproject"), "")
+	appconfig.setString("backendbaseurl", config.String("backendnaseurl"), "http://localhost:1313/")
+	appconfig.setString("serverbinding", config.String("serverbinding"), "127.0.0.1:3000")
+	appconfig.setString("gitcommand", config.String("gitcommand"), "git")
+
+	if appconfig.getBool("dev") {
+		fmt.Println("Development modus enabled (dev)")
 	}
+	fmt.Println("Working with (hugoproject): " + appconfig.HugoProject())
+	fmt.Println("Listening to (backendbaseUrl): " + appconfig.BackendBaseUrl())
+	fmt.Println("Service connects to (serverbinding): " + appconfig.ServerBinding())
+
+	return appconfig
+}
+
+func (appconfig AppConfigType) setBool(key string, value bool) {
+	appconfig.boolconfigs[key] = value
+}
+
+func (appconfig AppConfigType) getBool(key string) bool {
+	return appconfig.boolconfigs[key]
+}
+
+func (appconfig AppConfigType) setString(key string, value string, defaultvalue string) {
+	if value == "" {
+		value = defaultvalue
+	}
+	appconfig.stringconfigs[key] = value
+}
+
+func (appconfig AppConfigType) getString(key string) string {
+	return appconfig.stringconfigs[key]
 }
 
 func ensureLastSlash(text string) string {
@@ -68,15 +89,19 @@ func ensureLastSlash(text string) string {
 }
 
 func (appconfig AppConfigType) BackendBaseUrl() string {
-	return ensureLastSlash(appconfig.backendBaseUrl)
+	return ensureLastSlash(appconfig.getString("backendbaseurl"))
 }
 
 func (appconfig AppConfigType) HugoProject() string {
-	return ensureLastSlash(appconfig.hugoProject)
+	return ensureLastSlash(appconfig.getString("hugoproject"))
+}
+
+func (appconfig AppConfigType) ServerBinding() string {
+	return appconfig.getString("serverbinding")
 }
 
 func (appconfig AppConfigType) GitCommand() string {
-	return appconfig.gitCommand
+	return appconfig.getString("gitcommand")
 }
 
 // AppConfig singleton
