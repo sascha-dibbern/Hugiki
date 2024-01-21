17da27d65039ef8815bf207e6c78cea0753fe007
Sascha Dibbern <sascha-code@dibbern.info>
2024-01-21 21:05:29 +0100

    Refector. central URI definitions

--- hihandlers/definitions/uri-definitions.go
+++
@@ -1,7 +0,0 @@
-package definitions
-
-import "github.com/sascha-dibbern/Hugiki/hiview"
-
-const UriPage_EditContent = hiview.UriPage_EditContent
-const UriAction_ProxyContentPageBody = hiview.UriAction_ProxyContentPageBody
-const UriAction_UpdateContent = hiview.UriAction_UpdateContent
---
+++ hiuri/uridefinitions.go
@@ -0,0 +1,8 @@
+package hiuri
+
+const UriPage_Root = "/"
+const UriPage_Edit = "/hugiki/page/edit/"
+const UriPage_EditContent = UriPage_Edit + "content/"
+const UriPage_Config = "/hugiki/page/config/"
+const UriAction_ProxyContentPageBody = "/hugiki/action/proxy-content-page-body/"
+const UriAction_UpdateContent = "/hugiki/action/update/content/"
