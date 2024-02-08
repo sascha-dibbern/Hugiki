72d1274f3cbf8132f604fcc7ce169e34e1013aa7
Sascha Dibbern <sascha-code@dibbern.info>
2024-02-08 21:10:18 +0100

    Add CSS and other meta-data

--- hiview/pagegenerator/startpage.go
+++ hiview/pagegenerator/startpage.go
@@ -10,10 +10,14 @@ var startpagetemplate = `
 {{- define "startpagetemplate" -}}
 <html>
 <head>
-    
+	<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+	<meta http-equiv="Pragma" content="no-cache" />
+	<meta http-equiv="Expires" content="0" />
+	<link rel="stylesheet" href="/hugiki/static/hugiki.css">
 </head>
 <body>
 <script src="https://unpkg.com/htmx.org@1.9.10"></script>
+<h1 class="hi-header">Hugiki</h1>
 <div id="himode">
     <!-- startpage:startmode -->
 	{{ template "startmode" . }}
